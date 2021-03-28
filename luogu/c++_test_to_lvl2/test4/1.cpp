3923877cbaa06f4c07da1a52e00c6538ff7069e2
Eric Maddy <eric.maddy@gmx.com>
2021-03-28 18:35:39 -0400

    Sun Mar 28 18:35:39 EDT 2021

--- luogu/c++_test_to_lvl2/test4/1.cpp
+++ luogu/c++_test_to_lvl2/test4/1.cpp
@@ -1,7 +1,12 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
+
+void print_vector(vector<int> &v) {
+    for (auto i : v) cout << i << " ";
+    cout << endl;
+}
 
 int main() {
     int n;
@@ -9,15 +14,17 @@ int main() {
     vector<int> t;
     int temp;
     while (cin >> temp) t.push_back(temp);
-////////////////////////////////
+    ////////////////////////////////
     sort(t.begin(), t.end());
     // cout << t[0] + t.back();
     int mid = t.size() / 2;
     int max = 0;
-    vector<int> f (t.begin(), t.end() - mid);
-    vector<int> b (t.begin() + mid, t.end());
+    vector<int> f(t.begin(), t.end() - mid);
+    print_vector(f);
+    vector<int> b(t.begin() + mid, t.end());
     reverse(b.begin(), b.end());
-    for (int i = 0; i < f.size() - 1; i++) {
+    print_vector(b);
+    for (int i = 0; i < f.size(); i++) {
         if (f[i] + b[i] > max) max = f[i] + b[i];
     }
     cout << max;
