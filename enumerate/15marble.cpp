6508ce2bcb091fcb091ccd0fcf120a6596d92b7a
Kaitou-e <libbydu@macpro.home>
2021-04-19 18:51:00 -0400

    Mon Apr 19 18:51:00 EDT 2021

--- enumerate/15marble.cpp
+++ enumerate/15marble.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 int main() {
     int ans = 0;
-    for (int i = 1; i <= 12; i++) {
-        for (int j = i + 1; j <= 13; j++) {
-            for (int k = j + 1; k <= 14; k++) {
+    for (int i = 1; i <= 15; i++) {
+        for (int j = i + 1; j <= 15 - i; j++) {
+            for (int k = j + 1; k <= 15 - j; k++) {
                 if (k > j) ans ++;
             }
         }
