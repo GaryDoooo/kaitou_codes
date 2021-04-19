4f36411919493f1ed9dcbd11e7a2e8ab7b08ceec
Kaitou-e <libbydu@macpro.home>
2021-04-19 19:42:02 -0400

    Mon Apr 19 19:42:02 EDT 2021

--- enumerate/water_tank.cpp
+++ enumerate/water_tank.cpp
@@ -5,9 +5,9 @@ int main() {
     int water_loss = 3;
     int x = 0;
     while (1) {
-        if (x == 3) water_loss = 8;
+        if (x == 2) water_loss = 8;
         water -= water_loss;
-        if (x == 5) break;
+        if (x == 4) break;
         x += 1;
     }
     cout << water;
