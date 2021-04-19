642edc161f4b871e386f36d7d08d38b02cd1ea8e
Kaitou-e <libbydu@macpro.home>
2021-04-19 19:58:00 -0400

    Mon Apr 19 19:58:00 EDT 2021

---
+++ enumerate/circle_num.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main() {
+    int x = 0;
+    int c = 1;
+    vector<int> p {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    while (1) {
+        if (c == 3) {
+            int i = x % (p.size()) + 1;
+            cout << i << " ";
+            p.pop(i)
+            c == 1;
+        } else c ++;
+        x++;
+    }
+}
