5ff0aebdcae76bdecd1eaa8459ed51b7c5fd32e4
Kaitou-e <libbydu@macpro.home>
2021-04-19 18:52:01 -0400

    Mon Apr 19 18:52:01 EDT 2021

--- enumerate/15marble.cpp
+++ enumerate/15marble.cpp
@@ -5,7 +5,7 @@ int main() {
     for (int i = 1; i <= 15; i++) {
         for (int j = i + 1; j <= 15 - i; j++) {
             for (int k = j + 1; k <= 15 - j; k++) {
-                if (k > j) ans ++;
+                if (15 - (i + j + k) > k) ans ++;
             }
         }
     }
