144a5f78932bff76284dfd9983f21b4ad0509890
Kaitou-e <libbydu@macpro.home>
2021-04-19 20:27:00 -0400

    Mon Apr 19 20:27:00 EDT 2021

--- enumerate/coin_knight.cpp
+++ enumerate/coin_knight.cpp
@@ -3,12 +3,12 @@ using namespace std;
 int main() {
     int total = 0;
     int i = 1;
-    int day = 1;
+    int day = 0;
     while (1) {
         for (int c = 1; c <= i; c++) {
             total += i;
             day ++;
-            if (day >= 20) break;
+            if (day == 20) break;
         }
         i ++;
     }
