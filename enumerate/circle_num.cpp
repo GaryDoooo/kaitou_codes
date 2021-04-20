f2348b122d844967722bd068fb49931eb8278d68
Kaitou-e <libbydu@macpro.home>
2021-04-19 20:07:00 -0400

    Mon Apr 19 20:07:00 EDT 2021

--- enumerate/circle_num.cpp
+++ enumerate/circle_num.cpp
@@ -6,12 +6,14 @@ int main() {
     int c = 1;
     vector<int> p = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     while (1) {
-        if (c == 3) {
-            int i = x % 10 + 1;
-            cout << i << " ";
-            p[i] = -1;
-            c == 1;
-        } else c ++;
-        x++;
+        int i = x % 10 + 1;
+        if (p[i] != -1) {
+            if (c == 3) {
+                cout << i << " ";
+                p[i] = -1;
+                c == 1;
+            } else c ++;
+            x++;
+        }
     }
 }
