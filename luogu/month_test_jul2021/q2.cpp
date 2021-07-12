e75aa8b4fd79ff9a353ba6754b06af5785b8ef9a
Kaitou-e <libbydu@macpro.home>
2021-07-12 19:10:03 -0400

    Mon Jul 12 19:10:03 EDT 2021

---
+++ luogu/month_test_jul2021/q2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main() {
+    vector<int> scores;
+    int x;
+    while (cin >> x) {
+        scores.push_back(scores);
+    }
+    sort(scores.begin(), scores.end())
+    for (int i = 0; i < 3; i++)
+    }
