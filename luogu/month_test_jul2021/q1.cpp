c209667d14db1cad3a20d1b5412e94df2674085f
Kaitou-e <libbydu@macpro.home>
2021-07-12 18:38:01 -0400

    Mon Jul 12 18:38:01 EDT 2021

--- luogu/month_test_jul2021/q1.cpp
+++ luogu/month_test_jul2021/q1.cpp
@@ -1,3 +1,22 @@
 #include <iostream>
 int main() {
-}
+    int l, t;
+    int lp, tp, bp;
+    int la, ta;
+// 	cin >> l >> t >> lp >> tp >> bp;
+// 	bool bmwi=false;
+// 	if (lp + tp > bp*2)
+// 		bmwi = true;
+// 	bool nl, nt;
+// 	int la, ta
+// 	int total;
+// 	while (nl or nt){
+// 		if (nl and nt){
+// 			if (bmwi){
+// 				total += bp*2;
+// 				la += 1;
+// 				ta += 1;
+// 			}
+// 		}
+// 	}
+// }
