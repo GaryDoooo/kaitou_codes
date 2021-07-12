19615f2e73f200794abe40deb37f2a0df9634507
Kaitou-e <libbydu@macpro.home>
2021-07-12 19:20:01 -0400

    Mon Jul 12 19:20:01 EDT 2021

--- luogu/month_test_jul2021/q2.cpp
+++ luogu/month_test_jul2021/q2.cpp
@@ -10,9 +10,10 @@ int main() {
     }
     sort(scores.begin(), scores.end());
     int e = 0;
-    for (int i = 0; i < 3; i++) {
-        e += scores.pop_back();
-    }
+    for (i : scores) cout << i << " ";
+    //for (int i = 0; i < 3; i++) {
+    //e += scores.pop_back();
+    //}
     cout << e;
     return 0;
 }
