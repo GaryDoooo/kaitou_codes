a67c17adf91aa6e5bb3611451ab316475f53b06a
Kaitou-e <libbydu@macpro.home>
2021-07-12 19:22:01 -0400

    Mon Jul 12 19:22:01 EDT 2021

--- luogu/month_test_jul2021/q2.cpp
+++ luogu/month_test_jul2021/q2.cpp
@@ -8,7 +8,7 @@ int main() {
     while (cin >> x) {
         scores.push_back(x);
     }
-    sort(scores.begin(), scores.end());
+    sort(scores.begin(), scores.end() - 1);
     int e = 0;
     for (auto i : scores) cout << i << " ";
     //for (int i = 0; i < 3; i++) {
