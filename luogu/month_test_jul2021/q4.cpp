2d67f00500dc0348d1fdc6de038f328b778cee81
Kaitou-e <libbydu@macpro.home>
2021-07-12 20:56:01 -0400

    Mon Jul 12 20:56:01 EDT 2021

--- luogu/month_test_jul2021/q4.cpp
+++ luogu/month_test_jul2021/q4.cpp
@@ -4,9 +4,12 @@ using namespace std;
 int main() {
     int n, k;
     cin >> n >> k;
-    vector<int> res;
+    vector<int> a;
     int x;
     while (cin >> x) {
-        res.push_back(x);
+        a.push_back(x);
+    }
+    vector<int> b;
+    for (int i = 0; i < x.size(); i++) {
     }
 }
