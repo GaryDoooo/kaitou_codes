7bc6f0e9844d35d9f4348c756c493c87f6717a74
Kaitou-e <libbydu@macpro.home>
2021-07-12 20:58:02 -0400

    Mon Jul 12 20:58:02 EDT 2021

--- luogu/month_test_jul2021/q4.cpp
+++ luogu/month_test_jul2021/q4.cpp
@@ -9,12 +9,13 @@ int main() {
     while (cin >> x) {
         a.push_back(x);
     }
-    vector<int> b;
-    for (int i = 0; i < x.size() / 2; i++) {
-        b.push_back(a.back());
-        a.pop_back();
+    for (int i = )
+        // vector<int> b;
+        // for (int i = 0; i < x.size() / 2; i++) {
+        //     b.push_back(a.back());
+        //     a.pop_back();
+        // }
+        // for (auto i : a) {
+        //     if (cou)
+        //     }
     }
-    for (auto i : a) {
-        if (count())
-        }
-}
