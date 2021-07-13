864517a7430ddbe76d74d1b077b7f5c3e97ce933
Kaitou-e <libbydu@macpro.home>
2021-07-12 20:17:01 -0400

    Mon Jul 12 20:17:01 EDT 2021

--- luogu/month_test_jul2021/q3.cpp
+++ luogu/month_test_jul2021/q3.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 vector<int> convert_to_base(long long n, int base) {
     vector <int> res = {};
-    while (n > 1) {
+    while (n >= 1) {
         res.push_back(n % base);
         n /= base;
     }
