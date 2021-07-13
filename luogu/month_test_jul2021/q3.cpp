f863d9a60369d3a7f22128e331cbe2449ea90984
Kaitou-e <libbydu@macpro.home>
2021-07-12 20:37:01 -0400

    Mon Jul 12 20:37:01 EDT 2021

--- luogu/month_test_jul2021/q3.cpp
+++ luogu/month_test_jul2021/q3.cpp
@@ -35,9 +35,8 @@ int main() {
             if (fl) {
                 cout << "Yes" << endl;
                 fl = false;
-            } else {
-                cout << i << " ";
             }
+            cout << i << " ";
         }
     }
     if (fl) {
