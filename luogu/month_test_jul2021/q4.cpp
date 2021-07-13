786b9fd1201848eb766ac3d3c5880006d69e286c
Kaitou-e <libbydu@macpro.home>
2021-07-12 21:02:01 -0400

    Mon Jul 12 21:02:01 EDT 2021

--- luogu/month_test_jul2021/q4.cpp
+++ luogu/month_test_jul2021/q4.cpp
@@ -11,11 +11,11 @@ int main() {
     }
     vector<int> res;
     for (int i = 1; i <= k; i++) {
-        if (count(a.begin(), a.end(), i) > 0) {
+        if (count(a.begin(), a.end(), i) == 2) {
             res.push_back(i);
         }
     }
-    cout << res.size();
+    cout << res.size() << endl;
     for (auto i : res) {
         cout << i << " ";
     }
