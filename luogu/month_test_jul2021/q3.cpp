5e8aa324944af28c77d0afe49be3e925a6bbee66
Kaitou-e <libbydu@macpro.home>
2021-07-12 20:08:02 -0400

    Mon Jul 12 20:08:02 EDT 2021

--- luogu/month_test_jul2021/q3.cpp
+++ luogu/month_test_jul2021/q3.cpp
@@ -4,8 +4,14 @@ using namespace std;
 
 vector<int> convert_to_base(long long n, int base) {
     vector <int> res;
-    while ()
+    while (n > 1) {
+        res.push_back(n % base);
+        n /= base;
     }
+    return res;
+}
+bool hui_vecn(vector<int> n) {
+}
 int main() {
     long long x;
     cin >> x;
