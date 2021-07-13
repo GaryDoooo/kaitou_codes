36dfbce1a992452e58ab2e6cb98bd4b82028dfba
Kaitou-e <libbydu@macpro.home>
2021-07-12 20:22:01 -0400

    Mon Jul 12 20:22:01 EDT 2021

--- luogu/month_test_jul2021/q3.cpp
+++ luogu/month_test_jul2021/q3.cpp
@@ -18,10 +18,26 @@ int main() {
     long long x;
     cin >> x;
     vector<int> e = convert_to_base(x, 2);
-    for (auto i : e) cout << i << " ";
-    if (hui(e)) {
-        cout << "Yes" << endl;
-    } else {
-        cout << "No" << endl;
+    // for (auto i : e) cout << i << " ";
+    // if (hui(e)) {
+    //     cout << "Yes" << endl;
+    // } else {
+    //     cout << "No" << endl;
+    // }
+    bool fl = true;
+    for (int i = 2; i <= 16; i++ ) {
+        e = convert_to_base(x, i);
+        if (hui(e)) {
+            if (fl) {
+                cout << "Yes" << endl;
+                fl = false;
+            } else {
+                cout << i << " ";
+            }
+        }
     }
+    if (fl) {
+        cout << "No";
+    }
+    return 0;
 }
