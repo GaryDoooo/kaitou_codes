49d3c509dc3a416a90692d563ee04408f2045723
Kaitou-e <libbydu@macpro.home>
2021-07-16 09:34:01 -0400

    Fri Jul 16 09:34:01 EDT 2021

--- luogu/2.cpp
+++ luogu/2.cpp
@@ -8,8 +8,11 @@ int main() {
         int b = n / 100 % 10;
         int c = n / 10 % 10;
         int d = n % 10;
-        if (((a * 10 + b) + (c * 10 + d)) * ((a * 10 + b) + (c * 10 + d)))
+        if (((a * 10 + b) + (c * 10 + d)) * ((a * 10 + b) + (c * 10 + d))) {
             cout << a << b << c << d << endl;
+            return 0;
+        }
     }
+    cout << "No";
     return 0;
 }
