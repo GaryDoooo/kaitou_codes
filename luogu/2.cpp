368000d16c9a06ea72c6f56f054dda6fb2d92c07
Kaitou-e <libbydu@macpro.home>
2021-07-16 09:32:02 -0400

    Fri Jul 16 09:32:02 EDT 2021

---
+++ luogu/2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    for (int i = 2000; i <= n; i++) {
+        int a = n / 1000;
+        int b = n / 100 % 10;
+        int c = n / 10 % 10;
+        int d = n % 10;
+        if (((a * 10 + b) + (c * 10 + d)) * ((a * 10 + b) + (c * 10 + d)))
+            cout << a << b << c << d << endl;
+    }
+    return 0;
+}
