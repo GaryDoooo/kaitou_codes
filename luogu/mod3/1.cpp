d82a4c272f6bb25aa9020ccff215753912ef08e5
Kaitou-e <libbydu@macpro.home>
2021-08-13 17:49:02 -0400

    Fri Aug 13 17:49:02 EDT 2021

--- luogu/mod3/1.cpp
+++ luogu/mod3/1.cpp
@@ -1,4 +1,4 @@
-#include <bit/>
+#include <bits/stdc++.h>
 inline int read() {
     int x = 0, f = 1;
     char ch = getchar();
@@ -6,3 +6,7 @@ inline int read() {
     while (isdigit(ch)) {x = (x << 1) + (x << 3) + (ch ^ 48); ch = getchar();}
     return x * f;
 }
+int main() {
+    int n = read();
+    for (int i = 0; i < n; i++)
+    }
