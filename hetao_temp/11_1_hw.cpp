5eed1d465c39e140c52d6bbc103e88bc56741e5b
Kaitou-e <libbydu@macpro.home>
2021-08-29 15:32:00 -0400

    Sun Aug 29 15:32:00 EDT 2021

---
+++ hetao_temp/11_1_hw.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+const int N = 10005;
+vector<int> e[N];
+int n, u, v, x, de[N], cnt[N];
+void dfs(int u) {
+    for (int i = 0; i < e[u].size(); i++) {
+        int v = e[u][i];
+        de[v] = de[u] + 1;
+        // cnt[u]++;
+        dfs(v);
+    }
+}
+int main() {
+    cin >> n;
+    for (int i = 1; i <= n - 1; i++) {
+        cin >> u >> v;
+        e[u].push_back(v);
+    }
+    de[1] = 0;
+    cnt[0] = 1;
+    dfs(1);
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        cin >> x;
+        cout << de[x] << " " << e[x].size() + 1 << endl;
+    }
+    return 0;
+}
