2a48a3367d6667b741823debc92fa9858746c9a3
Kaitou-e <libbydu@macpro.home>
2022-05-15 18:21:01 -0400

    Sun May 15 18:21:01 EDT 2022

---
+++ hetao_temp/L16-8-4.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+int a[100005];
+
+bool cmp (int a, int b){
+    return a > b;
+}
+
+int main(){
+    int n, k;
+    cin >> n >> k;
+    for (int i = 1; i <= n; i++){
+        cin >> a[i];
+    }
+    sort(a + 1, a + n + 1, cmp);
+    int maxn = -1;
+    int maxt;
+    for (int i = 1; i <= n; i++){
+        int t = k;
+        int s = 0;
+        int l = i, r = i;
+        while (t > 0){
+            if (r == n and l == 1){
+                break;
+            }
+            
+            
+            if (((a[l-1] - a[i]) < (a[i] - a[r+1])) or r == n){
+                if (t < (a[l-1]-a[i])){
+                break;}
+                if (l-1 > 0){
+                    t -= a[l-1]-a[i];
+                    l--;
+                    s++;
+                }
+            }else{
+                if (t < (a[i] - a[r+1]) or r==n) break;
+                if (r + 1 <= n){
+                    t -= a[i] - a[r+1];
+                    r++;
+                    s++;
+                }
+            }
+        }
+        if (s > maxn){
+            maxn = s;
+            maxt = a[i];
+        }
+    }
+    cout << maxt << " " << maxn;
+    return 0;
+}
