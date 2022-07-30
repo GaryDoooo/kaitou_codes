dec7906030271f562fa886ab6e216062c398bd53
Eric Maddy <eric.maddy@gmx.com>
2022-07-30 14:42:44 -0400

    Sat Jul 30 14:42:44 EDT 2022

--- not_kaitou/tigao/a75.cpp
+++ not_kaitou/tigao/a75.cpp
@@ -2,48 +2,37 @@
 using namespace std;
 map<char, int> c2i;
 int m[55][55], n, done[55], dist;
-string inp1, inp2;
+char c1, c2;
 struct node {
     int dist, end;
 };
 bool operator<(const node& a, const node& b) { return a.dist < b.dist; }
 multiset<node> q;
+int to_index(char c) { return (c <= 'Z' ? c - 'A' : c - 'a' + 26); }
+//     if (c <= 'Z') return c - 'A';
+//     return c - 'a';
+// }
 int main() {
-    cin >> n;
+    scanf("%d\n", &n);
     memset(m, 127, sizeof(m));
-    // cout << m[0][0] << endl;
-    int j = 0;
-    for (char i = 'A'; i <= 'Z'; i++, j++) c2i[i] = j;
-    j = 26;
-    for (char i = 'a'; i <= 'z'; i++, j++) c2i[i] = j;
-    // cout << c2i['a'] << endl;
+    // int j = 0;
+    // for (char i = 'A'; i <= 'Z'; i++, j++) c2i[i] = j;
+    // j = 26;
+    // for (char i = 'a'; i <= 'z'; i++, j++) c2i[i] = j;
     for (int i = 0; i < n; i++) {
-        cin >> inp1 >> inp2 >> dist;
-        int n1 = c2i[inp1[0]], n2 = c2i[inp2[0]];
-        // cout << n1 << " " << n2 << " " << dist << endl;
-        // string subs = inp.substr(4);
-        // cout << subs << endl;
-        // int dist  = stoi(subs);
+        // cin >> inp1 >> inp2 >> dist;
+        scanf("%c %c %d\n", &c1, &c2, &dist);
+        //    int n1 = c2i[inp1[0]], n2 = c2i[inp2[0]];
+        int n1 = to_index(c1), n2 = to_index(c2);
+        cout << n1 << " " << n2 << " " << dist << endl;
         m[n1][n2] = min(m[n1][n2], dist);
         m[n2][n1] = min(m[n2][n1], dist);
     }
-    // node n1 = {1, 2}, n2 = {2, 3};
-    // if (n1 < n2)
-    //     puts("true");
-    // else
-    //     puts("false");
-    // for (int i = 0; i < 52; i++) {
-    //     cout << m[25][i] << " ";
-    // }
-    // cout << endl;
     q.insert({0, 25});
-    cout << q.size();
     while (!q.empty()) {
-        node now = *q.begin();
-        // cout << q.size() << " " << now.dist << " " << now.end << endl;
+        node now      = *q.begin();
         done[now.end] = 1;
         q.erase(q.begin());
-        // cout << q.size() << endl;
         if (now.end < 25) {
             cout << char('A' + now.end) << " " << now.dist << endl;
             return 0;
@@ -53,5 +42,4 @@ int main() {
             q.insert({m[now.end][next] + now.dist, next});
         }
     }
-    cout << "no found" << endl;
 }
