d095aac125702c748f194b45b9154f39cc1dbf7f
Eric Maddy <eric.maddy@gmx.com>
2022-07-30 14:57:29 -0400

    Sat Jul 30 14:57:29 EDT 2022

--- not_kaitou/tigao/a75.cpp
+++ not_kaitou/tigao/a75.cpp
@@ -1,6 +1,5 @@
 #include <bits/stdc++.h>
 using namespace std;
-map<char, int> c2i;
 int m[55][55], n, done[55], dist;
 char c1, c2;
 struct node {
@@ -9,22 +8,12 @@ struct node {
 bool operator<(const node& a, const node& b) { return a.dist < b.dist; }
 multiset<node> q;
 int to_index(char c) { return (c <= 'Z' ? c - 'A' : c - 'a' + 26); }
-//     if (c <= 'Z') return c - 'A';
-//     return c - 'a';
-// }
 int main() {
     scanf("%d\n", &n);
     memset(m, 127, sizeof(m));
-    // int j = 0;
-    // for (char i = 'A'; i <= 'Z'; i++, j++) c2i[i] = j;
-    // j = 26;
-    // for (char i = 'a'; i <= 'z'; i++, j++) c2i[i] = j;
     for (int i = 0; i < n; i++) {
-        // cin >> inp1 >> inp2 >> dist;
         scanf("%c %c %d\n", &c1, &c2, &dist);
-        //    int n1 = c2i[inp1[0]], n2 = c2i[inp2[0]];
         int n1 = to_index(c1), n2 = to_index(c2);
-        cout << n1 << " " << n2 << " " << dist << endl;
         m[n1][n2] = min(m[n1][n2], dist);
         m[n2][n1] = min(m[n2][n1], dist);
     }
