52ca89ad0f8ae4e19bad99e45d2ce13161a1f42f
Eric Maddy <eric.maddy@gmx.com>
2022-08-12 22:46:33 -0400

    Fri Aug 12 22:46:33 EDT 2022

--- not_kaitou/tigao/1411.cpp
+++ not_kaitou/tigao/1411.cpp
@@ -1,56 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 int const MOD = 1000000007;
-int n, m, k, a[1005], b[205], res;
-int f[1005][205];  // end at a[i] and b[i] substring res;
-int s[1005][205][205];
-vector<int> o[26];  // occurance position
-char input[1005];
-// struct node{
-//     int cnt,ap,bp;
-// };
-// queue<node> q;
+int n, m, k, u[201][201], f[201][201], init[201][201];
+char a[1005], b[205];
 int main() {
     scanf("%d%d%d", &n, &m, &k);
-    scanf("%s\n", input);
-    for (int i = 0; i < n; i++) {
-        char c = input[i];
-        a[i]   = c - 'a';
-        o[a[i]].push_back(i);
-    }
-    scanf("%s\n", input);
-    for (int i = 0; i < m; i++) {
-        char c = input[i];
-        b[i]   = c - 'a';
-    }
-    int last_occurrence = -1;
-    for (int start = 0; start < m; start++) {
-        int cnt[m], this_occurrence = 1e9;
-        memset(cnt, 0, sizeof(cnt));
-        for (int a_pos : o[b[start]]) {
-            if (n - a_pos < m - start or a_pos <= last_occurrence) continue;
-            this_occurrence        = min(this_occurrence, a_pos);
-            s[a_pos][start][start] = ++cnt[start];
-            for (int end = start + 1; end < m; end++) {
-                if (a[a_pos + end - start] == b[end])
-                    s[a_pos][start][end] = ++cnt[end];
-                else
-                    break;
-            }
+    scanf("%s\n", a + 1);
+    scanf("%s\n", b + 1);
+    f[0][0] = init[0][0] = 1;
+    for (register int ap = 1; ap <= n; ap++) {
+        for (register int bp = min(ap, m); bp >= 1; bp--) {
+            if (b[bp] == a[ap])
+                for (register int c = min(bp, k); c > 0; c--) {
+                    u[bp][c] = (u[bp - 1][c] + f[bp - 1][c - 1]) % MOD;
+                    f[bp][c] = (f[bp][c] + u[bp][c]) % MOD;
+                }
+            else
+                memcpy(u, init, sizeof(u));
         }
-        last_occurrence = this_occurrence;
     }
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            for (int k = 0; k < m; k++) cout << s[i][j][k] << " ";
-            cout << endl;
-        }
-        cout << endl;
-    }
-    for (int cut=0;cut<k; cut++){
-        if (cut<k-1){
-        for (int p=0; p<m; p++){
-
-            
+    printf("%d\n", f[m][k]);
 }
-
