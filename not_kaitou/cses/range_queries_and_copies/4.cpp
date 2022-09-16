e5f579dbb8bc78789bdaee902ab39a627d5ca05d
Eric Maddy <eric.maddy@gmx.com>
2022-09-16 00:04:49 -0400

    Fri Sep 16 00:04:49 EDT 2022

---
+++ not_kaitou/cses/range_queries_and_copies/4.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+#define ll unsigned long long
+using namespace std;
+int n, q, a, b, c, x[212345], father[212345], done[212345], cnt = 1;
+int create_time[212345];
+ll s[212345], res;
+struct ch {
+    int time_stamp, a, x;
+};
+bool operator<(const ch& a, const ch& b) { return a.a < b.a; }
+set<ch> changes[212345];
+ll seg[200002][202];
+ll get(int a, int b, int c) {
+    if (b>c) return 0;
+    ll res       = 0;
+    int deadline = 0;
+    while (a) {
+        for (auto it = changes[a].lower_bound({0, b, 0});
+             it != changes[a].end(); it++)
+            if (it->a > c)
+                break;
+            else if (it->time_stamp < deadline)
+                continue;
+            else if (q != done[it->a]) {
+                res += it->x - x[it->a];
+                done[it->a] = q;
+            }
+        deadline = create_time[a];
+        a        = father[a];
+    }
+    return res;
+}
+int main() {
+    scanf("%d%d", &n, &q);
+    for (int i = 1; i <= n; i++) {
+        scanf("%d", x + i);
+        s[i] = s[i - 1] + x[i];
+    }
+    memset(done, 127, sizeof(done));
+    create_time[1] = q + 1;
+    while (q--) {
+        scanf("%d", &a);
+        if (a == 1) {
+            scanf("%d%d%d", &a, &b, &c);
+            auto it = changes[a].lower_bound({0, b, 0});
+            if (it != changes[a].end())
+                if (it->a == b) seg[a][b >> 10] -= it->x;
+            changes[a].insert({q, b, c - x[b]});
+            seg[a][b >> 10] += c - x[b];
+        } else if (a == 2) {
+            scanf("%d%d%d", &a, &b, &c);
+            // cout << a << " " << b << " " << c << endl;
+            res = s[c] - s[b - 1];
+            ll l=b>>10, r=c>>10;
+
+            printf("%lld\n", res);
+        } else {
+            scanf("%d", &a);
+            father[++cnt]    = a;
+            create_time[cnt] = q;
+            memcpy(seg[cnt], seg[a], sizeof(ll) * 201);
+        }
+    }
+}
