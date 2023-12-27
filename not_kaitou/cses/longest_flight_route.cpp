98af13773220da0abb07d90dab4dc43f80b4d2fd
Eric Maddy <eric.maddy@gmx.com>
2023-12-27 08:49:12 -0500

    Wed Dec 27 08:49:12 EST 2023

--- not_kaitou/cses/longest_flight_route.cpp
+++ not_kaitou/cses/longest_flight_route.cpp
@@ -1,8 +1,10 @@
-#include <stdio.h>
+#include <bits/stdc++.h>
+using namespace std;
 #define N 112345
 #define M 212345
-int n, m, u, v, e[M * 2], d[N], p[N], f[N], e_cnt, in_q[N], q[M], head = 1,
-                                                                  tail;
+int n, m, u, v, e[M * 2], d[N], p[N], f[N], e_cnt, in_degree[N], tail, qq[N];
+queue<int> q;
+
 int main() {
     scanf("%d%d", &n, &m);
     while (m--) {
@@ -10,33 +12,34 @@ int main() {
         e[++e_cnt] = p[u];
         e[++e_cnt] = v;
         p[u]       = e_cnt;
+        in_degree[v]++;
     }
-    q[++tail] = 1;
-    while (head <= tail) {
-        u       = q[head++];
-        in_q[u] = 0;
+    memset(d, 128, sizeof(d));
+    d[1] = 0;
+    for (int i = 1; i <= n; i++)
+        if (in_degree[i] == 0) q.push(i);
+    while (q.size()) {
+        u = q.front();
+        q.pop();
         for (int i = p[u]; i; i = e[i]) {
             v = e[i--];
-            if (d[v] < d[u] + 1) {
+            if (d[v] <= d[u]) {
                 d[v] = d[u] + 1;
                 f[v] = u;
-                if (!in_q[v]) {
-                    q[++tail] = v;
-                    in_q[v]   = 1;
-                }
             }
+            if (--in_degree[v] == 0) q.push(v);
         }
     }
-    if (d[n] == 0)
+    if (d[n] <= 0)
         printf("IMPOSSIBLE");
     else {
         u    = n;
         tail = 0;
         while (u) {
-            q[++tail] = u;
-            u         = f[u];
+            qq[++tail] = u;
+            u          = f[u];
         }
         printf("%d\n", tail);
-        while (tail) printf("%d ", q[tail--]);
+        while (tail) printf("%d ", qq[tail--]);
     }
 }
