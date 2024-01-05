423be61b9bb4996b38be6cd928133872fc59b54c
Eric Maddy <eric.maddy@gmx.com>
2024-01-04 22:22:43 -0500

    Thu Jan  4 22:22:43 EST 2024

--- not_kaitou/cses/distinct_routes.cpp
+++ not_kaitou/cses/distinct_routes.cpp
@@ -1,8 +1,3 @@
-// Though passed the tests online but gives wrong results with the input below
-// need revise a new version.
-// 6 7
-// 1 3 2 4 5 6 2 5 1 2 3 5 4 6
-
 #include <bits/stdc++.h>
 using namespace std;
 int n, m;
@@ -11,11 +6,14 @@ struct edge {
 };
 edge e[1001];
 unordered_set<int> ee[501];
+// vector<int> ee[501];
 int vis[501], e_vis[1001], res, cur_vis_e[1001];
 int distinguish_path_cnt = 0;
 vector<vector<int>> paths;
+vector<int> cur_path;
 
 int find_path(int u) {
+    cur_path.push_back(u);
     if (u == n) return 1;
     vis[u] = 1;
     for (int ei : ee[u]) {
@@ -27,10 +25,12 @@ int find_path(int u) {
             v = e[ei].u;
         if (vis[v]) continue;
         cur_vis_e[ei] = 1;
+        cout << "path " << e[ei].u << " -> " << e[ei].v << endl;
         if (find_path(v)) return 1;
         // cur_vis_e[ei] = 0;
     }
     vis[u] = 0;
+    cur_path.pop_back();
     return 0;
 }
 
@@ -55,30 +55,29 @@ int main() {
         int u, v;
         scanf("%d %d", &u, &v);
         e[i].u = u, e[i].v = v;
+        // ee[u].push_back(i);
         ee[u].insert(i);
-        ee[v].insert(i);
+        // ee[v].insert(i);
     }
+    for (int i : ee[2]) cout << e[i].u << "->" << e[i].v << endl;
     do {
         memset(vis, 0, sizeof(vis));
         memset(cur_vis_e, 0, sizeof(cur_vis_e));
+        cur_path.clear();
         res = find_path(1);
         if (res) {
+            paths.push_back(cur_path);
             vector<int> path;
             for (int i = 1; i <= m; i++)
                 if (cur_vis_e[i]) path.push_back(i), e_vis[i] = 1;
-            paths.push_back(path);
+            // paths.push_back(path);
             distinguish_path_cnt += res;
         }
     } while (res);
-    // cout << paths.size() << endl;
-    // for (auto p : paths) {
-    //     for (auto e : p) cout << e << " ";
-    //     cout << endl;
-    // }
     cout << distinguish_path_cnt << endl;
-    vector<int> edge_cut;
-    while (distinguish_path_cnt--)
-        edge_cut.push_back(find_to_cut(distinguish_path_cnt));
-    for (int i : edge_cut) printf("%d %d\n", e[i].u, e[i].v);
+    for (auto path : paths) {
+        printf("%ld\n", path.size());
+        for (int i : path) printf("%d ", i);
+        printf("\n");
+    }
 }
-
---
+++ not_kaitou/cses/distinct_routes2.cpp
@@ -0,0 +1,101 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct edge {
+    int u, v;        // u->v
+    int start_node;  // node #, if 0 the edge is availalbe
+    int fa;          // prev step, first step's fa is 0;
+    int step_num;    // count step on this path
+    int ee_i;  // index of edge on node u, for continue to try untried edges;
+               // need test if to try all edges or untried ones;
+};
+edge e[1005];
+vector<int> path_start;  // node # of the nodes 1 step from "1" starting points
+int path_tail[505];      // pointer to the step chain idx node #
+int n, m;
+vector<int> ee[505];  // edge idx from one node idx is node #
+int vis_e_cur[1005];  // if the edge is visisted in this round
+// int vis_node_cur_path[505];  // if the node is visited in this path before
+
+void mark_new_path(int prev_edge) {
+    path_tail[e[prev_edge].start_node] = prev_edge;
+    int start_node                     = e[prev_edge].start_node;
+    for (int i = 1; i <= m; i++)
+        if (e[i].start_node == start_node) e[i].start_node = 0;
+    // clean all the old marks
+    for (int i = prev_edge; i; i = e[i].fa)
+        e[i].start_node = start_node;  // trace the new path mark
+}
+
+int found_path(const int prev_edge, const int start_ee_i) {
+    if (e[prev_edge].v == n) {
+        mark_new_path(prev_edge);
+        return 1;
+    }
+    //
+    int vis_node_cur_path[505], cur_node = e[prev_edge].v,
+                                start_node = e[prev_edge].start_node;
+    memset(vis_node_cur_path, 0, sizeof(vis_node_cur_path));
+    for (int i = prev_edge; i; i = e[i].fa) vis_node_cur_path[e[i].v] = 1;
+    vis_node_cur_path[1] =
+        1;  // Get all the previous nodes on the path to prevent loops
+            //
+    for (int i = start_ee_i; i < ee[cur_node].size(); i++) {
+        int ei = ee[cur_node][i];
+        if (vis_e_cur[ei]) continue;
+        if (vis_node_cur_path[e[ei].v]) continue;
+        // if (e[ei].start_node > start_node)
+        // continue;  // if the edge belongs to future paths
+        vis_e_cur[ei] = 1;
+        if (e[ei].start_node == start_node) {
+            e[ei].fa = prev_edge;
+            mark_new_path(
+                path_tail[start_node]);  // the new path connected to old tails
+            return 1;
+        }
+        // Here the e[ei]'s start node must less than current start node,
+        // It's previous paths
+        if (e[ei].start_node > 0 and
+            e[ei].start_node !=
+                start_node)  // The edge is not free, but on an old path
+            if (found_path(e[ei].fa,  // Go back one step at this old path
+                           e[ei].ee_i +
+                               1  // start trying from the next edge on the node
+                           ) == 0)
+                continue;  //
+        // Up to here the edge is free to be added to current path
+        e[ei].start_node = start_node;
+        e[ei].fa         = prev_edge;
+        if (found_path(ei, 0)) return 1;
+    }
+    return 0;
+}
+
+int main() {
+    scanf("%d %d", &n, &m);
+    for (int i = 1; i <= m; i++) {
+        scanf("%d %d", &e[i].u, &e[i].v);
+        e[i].ee_i = ee[e[i].u].size();
+        ee[e[i].u].push_back(i);
+    }
+    for (int i = 0; i < ee[1].size(); i++)
+        path_start.push_back(e[ee[1][i]].v);  // all the nodes connected to 1
+    int path_cnt = 0;
+    sort(path_start.begin(),
+         path_start.end());  // only changes the paths smaller than current
+    for (int start_node : path_start) {
+        // memset(vis_node_cur_path, 0, sizeof(vis_node_cur_path));
+        memset(vis_e_cur, 0, sizeof(vis_e_cur));
+        int prev_edge = 0;
+        for (int i : ee[1]) {
+            if (e[i].v == start_node) prev_edge = i;
+            // vis_node_cur_path[e[i].v] = 1;
+            vis_e_cur[i] = 1;
+        }
+        // vis_node_cur_path[1]    = 1;
+        e[prev_edge].start_node = start_node;
+        e[prev_edge].step_num   = 1;
+        path_cnt += found_path(prev_edge, 0);
+    }
+    printf("%d\n", path_cnt);
+}
+
